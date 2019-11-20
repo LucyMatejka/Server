ea853dce3020e21abfd098a644003c545e23d766
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-11-20 10:03:17 +0000

    http servers

--- client/my_server.rb
+++ client/my_server.rb
@@ -2,10 +2,11 @@ require 'socket'
 
 server = TCPServer.new(2345)
 
+while true
 socket = server.accept
 
 socket.puts "HELLO CLIENT WHAT IS YOUR NAME?"
 name = socket.gets.chomp
 socket.puts "THAT'S A LOVELY NAME #{name}"
-
+end
 socket.close
